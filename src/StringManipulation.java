172a26b911b38713420b80957036d43aab26042d
Christian A. Gulfan <christiangulfan366@gmail.com>
2026-04-01 13:38:54 +0800

    Refactor StringManipulation class to use constructor

--- src/StringManipulation.java
+++ src/StringManipulation.java
@@ -1,83 +1,78 @@
 import java.util.Arrays;
 import java.util.Scanner;
+
 public class StringManipulation {
-    public static void stringSorting(Scanner scanner){
-        //user input
+
+    private Scanner scanner;
+
+    public StringManipulation(Scanner scanner){
+        this.scanner = scanner;
+    }
+
+    public void stringSorting(){
         System.out.print("Enter a word: ");
         String word = scanner.nextLine();
 
-        //turn it into an array
         char a[] = word.toCharArray();
 
         for(int i = 0; i<a.length; i++){
             for(int j = 0; j<a.length-1; j++){
-                if(a[j] > a[j+1]){char temp = a[j]; a[j] = a[j+1]; a[j+1] = temp;}
+                if(a[j] > a[j+1]){
+                    char temp = a[j];
+                    a[j] = a[j+1];
+                    a[j+1] = temp;
+                }
             }
         }
 
-        //output
         System.out.print("\nOutput: ");
         for(int i = 0; i<a.length; i++) System.out.printf("%c", a[i]);
         System.out.println();
     }
 
-    public static void strArraySorting(Scanner scanner){
-        //user input
+    public void strArraySorting(){
         System.out.print("Enter a word: ");
         String word = scanner.nextLine();
 
-        //turn it into an array
         char a[] = word.toCharArray();
 
-        //array sort built-in
         Arrays.sort(a);
         String sorted = new String(a);
 
-        //output
         System.out.printf("\nOutput: %s\n\n", sorted);
     }
 
-    public static void reverseString(Scanner scanner){
+    public void reverseString(){
         String reversedWord = "";
 
-        //user input
         System.out.print("Enter a word: ");
         String word = scanner.nextLine();
 
-        //reversing the word using its index
-        //take note index starts at 0
         for(int i = word.length()-1; i>=0; i--){
             reversedWord += word.charAt(i);
         }
 
-        //result
         System.out.printf("\nOutput: %s\n\n", reversedWord);
     }
 
-    public static void palindromeStr(Scanner scanner) {
+    public void palindromeStr() {
         String reversedWord = "";
 
-        //user input
         System.out.print("Enter a word: ");
         String word = scanner.nextLine();
-        String ogWord = word; //made a copy for later comparison
+        String ogWord = word;
 
-        //reversing the word using its index
-        //take note index starts at 0
         for (int i = word.length() - 1; i >= 0; i--) {
             reversedWord += word.charAt(i);
         }
 
-        //output
         System.out.printf("\nOutput: %s\n", reversedWord);
 
-        //check if palindrome or not
         if (reversedWord.equals(ogWord)) System.out.println("Palindrome!");
         else System.out.println("NOT Palindrome!");
     }
 
-    public static void stringManipulation(Scanner scanner){
-        //menu
+    public void stringManipulation(){
         int choice;
 
         do{
@@ -90,20 +85,20 @@ public class StringManipulation {
             System.out.print("Your choice: ");
 
             choice = scanner.nextInt();
-            scanner.nextLine(); //to remove newline
+            scanner.nextLine(); // newline fix
 
             switch(choice){
                 case 1:
-                    stringSorting(scanner);
+                    stringSorting();
                     break;
                 case 2:
-                    strArraySorting(scanner);
+                    strArraySorting();
                     break;
                 case 3:
-                    reverseString(scanner);
+                    reverseString();
                     break;
                 case 4:
-                    palindromeStr(scanner);
+                    palindromeStr();
                     break;
                 case 5:
                     System.out.println("Returning to main menu...");
